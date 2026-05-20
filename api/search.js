0cff7e74b62fbe1ad421ef59e092c0d7f480f828
fallen-mizu <tohsakarin756@gmail.com>
2026-05-20 22:33:25 +0700

    Create search.js

---
+++ api/search.js
@@ -0,0 +1,25 @@
+import ytSearch from 'yt-search';
+
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    const { q } = req.query;
+    if (!q) return res.status(400).json({ status: false, message: "Query q diperlukan" });
+
+    try {
+        const results = await ytSearch(q);
+        const songs = results.videos.slice(0, 5).map(video => ({
+            id: video.videoId,
+            title: video.title,
+            duration: video.timestamp,
+            thumbnail: video.thumbnail,
+            author: video.author.name
+        }));
+        
+        return res.status(200).json({ status: true, results: songs });
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message });
+    }
+}
+    
